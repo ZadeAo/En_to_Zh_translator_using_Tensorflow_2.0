f8b722557ef4533e3f3e2a85bee1df138c7227d1
yusanshi <yulei2018@gmail.com>
2019-08-18 18:56:31 +0800

    fix a bug in encoding

--- tools/normalize_config_importing.py
+++ tools/normalize_config_importing.py
@@ -4,12 +4,13 @@ import os
 
 
 def normalize():
-    config = re.findall(r'\w+ {0,1}=', io.open('config.py').read())
+    config = re.findall(
+        r'\w+ {0,1}=', io.open('config.py', 'r', encoding='utf-8').read())
     config = [re.findall(r'\w+', w)[0] for w in config]
 
     for file in os.listdir('./'):
         if os.path.splitext(file)[-1] == '.py' and os.path.splitext(file)[0] != 'config':
-            f = io.open(file, 'r')
+            f = io.open(file, 'r', encoding='utf-8')
             all_text = f.read()
             lines = all_text.split('\n')
 
@@ -31,7 +32,7 @@ def normalize():
                     new_text = '\n'.join(
                         [l if l != line else new for l in lines])
 
-                    f = io.open(file, 'w')
+                    f = io.open(file, 'w', encoding='utf-8')
                     f.write(new_text)
                     f.close
                     break
